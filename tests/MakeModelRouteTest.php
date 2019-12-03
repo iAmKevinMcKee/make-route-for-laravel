f14e862679b8d2b8701977a955104294bd3c7130
Kevin <kevin@intellow.com>
2019-12-03 11:31:20 -0600

    wip

---
+++ tests/MakeModelRouteTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Intellow\MakeRouteForLaravel\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MakeModelRouteTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
